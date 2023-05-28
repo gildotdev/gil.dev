ceddcd725ac6349adc8922826ecb6cee248332ae
Gil Creque <gilcreque@gmail.com>
2023-05-28 21:28:42 +0000

    Add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from "astro/config";
-// https://astro.build/config
 import analogjsangular from "@analogjs/astro-angular";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
