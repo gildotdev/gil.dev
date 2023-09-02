366df3c25898b7710f964c7ced6681e56d614f95
Gil Creque <gilcreque@gmail.com>
2023-09-02 18:31:31 +0000

    Upgrade to Astro V3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), analogjsangular(), mdx()],
-  markdown: { shikiConfig: { theme: "css-variables" } },
-  experimental: {
-    assets: true
-   }
+  markdown: { shikiConfig: { theme: "css-variables" } }
 });
