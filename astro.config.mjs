af3afe7b9aa9d55f9d5c701baf73e9df5c67190e
Gil Creque <gilcreque@gmail.com>
2024-09-15 04:11:40 +0000

    Working component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
-import analogjsangular from "@analogjs/astro-angular";
+import angular from "@analogjs/astro-angular";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), analogjsangular(), mdx()],
   markdown: { shikiConfig: { theme: "css-variables" } },
   assetsInclude: ["**/*.vtt"],
   server: { port: 1716, host: true },
+  integrations: [
+    tailwind(),
+    angular({
+      vite: {
+        inlineStylesExtension: "scss|sass|less",
+        ssr: {
+          // transform these packages during SSR. Globs supported
+          noExternal: ["@rx-angular/**"],
+        },
+      },
+    }),
+    mdx(),
+  ],
 });
