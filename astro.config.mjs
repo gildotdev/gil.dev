254f5202fd798e7a1cfdeca7c0d6c07aa50eaa4a
Gil Creque <gilcreque@gmail.com>
2025-01-04 03:23:03 +0000

    Set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   markdown: { shikiConfig: { theme: "css-variables" } },
   assetsInclude: ["**/*.vtt"],
   server: { port: 1716, host: true },
