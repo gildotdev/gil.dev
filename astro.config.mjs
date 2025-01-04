6a8f26b62390f324ce86c057666faa625ed9e07c
gil.dev <gilcreque@gmail.com>
2025-01-04 12:22:28 -0500

    Add microdotblog (#5)
    * Working component
    
    * Replaced Micro Dot Blog Angular Component with Astro Component
    
    * Set output to static
    
    * Added `/posts`
    
    * Replaced colophon icon
    
    * Fix up the footer menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
-import analogjsangular from "@analogjs/astro-angular";
+import angular from "@analogjs/astro-angular";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), analogjsangular(), mdx()],
+  output: "static",
   markdown: { shikiConfig: { theme: "css-variables" } },
   assetsInclude: ["**/*.vtt"],
   server: { port: 1716, host: true },
+  integrations: [
+    tailwind(),
+    angular({
+      vite: {
+        inlineStylesExtension: "scss|sass|less",
+        ssr: {
+          // transform these packages during SSR. Globs supported
+          noExternal: ["@rx-angular/**"],
+        },
+      },
+    }),
+    mdx(),
+  ],
 });
