038381abd6e06c548ec9740fa5be2e7d9f0f20e4
Gil Creque <gilcreque@gmail.com>
2025-03-17 17:20:41 -0400

    Removed Angular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import angular from "@analogjs/astro-angular";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -9,17 +8,5 @@ export default defineConfig({
   markdown: { shikiConfig: { theme: "css-variables" } },
   assetsInclude: ["**/*.vtt"],
   server: { port: 1716, host: true },
-  integrations: [
-    tailwind(),
-    angular({
-      vite: {
-        inlineStylesExtension: "scss|sass|less",
-        ssr: {
-          // transform these packages during SSR. Globs supported
-          noExternal: ["@rx-angular/**"],
-        },
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind(), mdx()],
 });
